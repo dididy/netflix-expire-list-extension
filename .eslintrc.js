39fe21e1feb3aa2d847f5fd7900d04fb741458c6
dididy <dev.yongjaelee@gmail.com>
2020-11-16 03:25:09 +0900

    env: eslintrc.js 설정 추가

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: ["plugin:react/recommended", "airbnb"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+    context: "readonly",
+    Feature: "readonly",
+    Scenario: "readonly",
+    actor: "readonly",
+    given: "readonly",
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "react/jsx-filename-extension": [0],
+    "react/require-default-props": 0,
+    "no-nested-ternary": 0,
+  },
+};
